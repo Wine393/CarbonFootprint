239712b9541d24dd437390fbe1d2e732040eeaf8
Wine393 <winelinkar77@gmail.com>
2026-05-02 13:37:45 +0630

    Create TransportActivity class for CO2 reporting
    Add TransportActivity class with documentation for CO2 calculation

---
+++ TransportActivity.java
@@ -0,0 +1,8 @@
+/**
+ * Class: TransportActivity
+ * Extends CarbonSource, implements CarbonCalculable + Reportable.
+ *
+ * Fields  : vehicleType, distanceKM, passengers
+ * Formula : (distanceKM x factor) / passengers  → kg CO2
+ * Types   : car | bus | train | plane | motorbike
+ */
